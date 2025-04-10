4207c2a25c45ee9e4786f56d62a1a0fc3759849e
Zamir Karimov <159486333+ZamirKarimov@users.noreply.github.com>
2025-04-10 16:07:34 +0000

    Start

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function(start){
-  
+var cloths = ["Jacket + Warm Pants", "Spring Jacket + Jeans", "T-Shirt + Shorts", "Hoodie + Pants"]
+var seasons = ["winter", "spring", "summer", "fall"]
+function start(){
+    var input = readLine("Hey! Give me a season that you have right now and I will give you clothing that match your weather! ");
+    var result = season(input);
+    console.log(result)
+}
+function season(input){
+    for(var i = 0; i < seasons.length; i++){
+        if(input.toLowerCase() === seasons[i]){
+            console.log(input)
+            return cloths[i];
+        }
+    }
 }
